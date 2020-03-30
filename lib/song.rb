cdf94c0652de2dde49cd06fcbe820c200935b3c1
Crystal Schooling <bytesizemagic@gmail.com>
2020-03-30 22:04:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,11 +6,14 @@ class Song
 
   def initialize(name)
     @name = name
-    @@all << name
+    @@all << self
   end
 
   def self.all
     @@all
   end
 
+  def artist_name
+    self.artist.name
+  end
 end
