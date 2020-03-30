4825c1bdb5ed7535a6f8f4d8dede89e21523ce80
Crystal Schooling <bytesizemagic@gmail.com>
2020-03-30 22:01:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,16 +4,14 @@ class Artist
 
   def initialize(name)
     @name = name
-    @songs = []
   end
 
   def add_song(song)
-    @songs << song
     song.artist = self
   end
 
-  def add_song_by_name(song)
-    song = Song.new(song)
+  def add_song_by_name(name)
+    song = Song.new(name)
     add_song(song)
   end
 
