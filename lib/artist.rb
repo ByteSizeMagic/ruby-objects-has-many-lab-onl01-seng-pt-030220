e7f03297e87278e70623929be7f3513b86d28d6b
Crystal Schooling <bytesizemagic@gmail.com>
2020-03-30 21:57:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,15 +2,14 @@ class Artist
 
   attr_accessor :name
 
-  @@songs = []
-
   def initialize(name)
     @name = name
-
+    @songs = []
   end
 
   def add_song(song)
-    @@songs << song
+    @songs << song
+    song.artist = self
   end
 
   def add_song_by_name(song)
